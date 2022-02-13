e60a402ecba303ea8a746f94b084c18392abe5d3
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2022-02-13 03:38:18 -0800

    Added Status HTML & Prepared For Deployment Settings

--- cashbuddy/settings.py
+++ cashbuddy/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jyeh054^nshc25o-n+7#&g4lf@f2)k)gjx@13jiow-*6g$c@4g'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['cash-buddy.herokuapp.com', '127.0.0.1']
 
